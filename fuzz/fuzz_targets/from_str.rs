3033873b46e1adfd1737caf35fa739a2d0e6c7db
Markus Mayer <widemeadows@gmail.com>
2025-02-22 11:54:26 +0100

    Fix spellcheck complaining about serde

--- fuzz/fuzz_targets/from_str.rs
+++ fuzz/fuzz_targets/from_str.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
 use docker_image::DockerImage;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(str) = std::str::from_utf8(data) {
